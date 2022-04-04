0ff3a4a60e372a04006e1387335887dc208ea806
anaberger <ana.berger@betrybe.com>
2022-04-04 09:17:00 -0300

    ex 5

---
+++ ex5.js
@@ -0,0 +1,4 @@
+//Escreva uma função que calcule a velocidade final de um objeto em queda livre
+//dado o tempo que levou para atingir o solo. 
+//A fórmula para o cálculo é velocidade = tempo * gravidade. 
+//Considere a gravidade constante de 9.8.
